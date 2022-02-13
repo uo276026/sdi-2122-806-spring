413811ea9e8f56ae2faebc0a524c6bae627ed187
Lara <uo276026@uniovi.es>
2022-02-13 15:35:03 +0100

    Initial commit

---
+++ src/main/java/com/uniovi/sdi2122806spring/Sdi2122806SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122806spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122806SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122806SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2122806spring/Sdi2122806SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122806spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122806SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
